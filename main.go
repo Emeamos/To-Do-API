ec20157720c4a6fdbcdafa76e2e208221adc2ee5
Emeamos <Emediongamos.engr@gmail.com>
2023-06-12 17:06:10 +0100

    adding database

--- main.go
+++ main.go
@@ -1,2 +1,14 @@
 package main
 
+type todo struct{
+	ID					string	`json:"id"`
+	Item				string	`json:"title"`
+	Completed			bool	`json:"complete"`
+}
+
+var todos = []todo{
+	{ID:"1", Item:"read movie", Completed: false},
+	{ID:"2", Item:"play game", Completed: false},
+	{ID:"3", Item:"record movie", Completed: false},
+
+}
